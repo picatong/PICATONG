cf555dcdb88c345a4ed85f168934ad94652105ec
picatong <71311483+picatong@users.noreply.github.com>
2020-09-17 04:37:34 +0900

    beta 1.2.1

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  var button = document.getElementByID("night");
+  button.addEventListener("night", displayTime. false);
+  button.addEventListener("night", function(e) { console.log("second listener:" + e); }, false);
+}
